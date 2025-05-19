2a2b352de9c5c0c8b62750bee44b08e030e19667
vo55my <dhaifullahhilmy@gmail.com>
2025-05-19 18:29:31 +0700

    Update footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -2,7 +2,7 @@
   <footer class="bg-white dark:bg-gray-900">
     <div class="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
       <div class="md:flex md:justify-between">
-        <div class="mb-6 md:mb-0 w-80">
+        <div class="mb-6 md:mb-0 md:w-80">
           <a href="#" class="flex items-center">
             <img
               src="/images/Slide/Logo.png"
